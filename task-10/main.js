202431f7454620c0a54dae15d83b07c23c8f813c
Aslan Mammadov <105041099+MammadovAslan@users.noreply.github.com>
2022-06-28 11:45:20 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -1,3 +1,4 @@
+
 const prompt = require('prompt');
 prompt.start();
 
@@ -5,16 +6,12 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     let str = result.input.split('');
-    //I am programming on Javascript. 
+    //I am programming on Javascript.
     
-    for(let i = 0;i<str.length;i++){
-        if(str[i] === ' '){
-            console.log(str.indexOf(str[i]));
-            console.log(str.lastIndexOf(str[i]));
-            break
-        }else{
-            console.log(-1);
-            break
-        }
+    if(str.includes(' ')){
+        console.log(str.indexOf(' '));
+        console.log(str.lastIndexOf(' '));
+    }else{
+        console.log(str.indexOf(' '));
     }
 });
