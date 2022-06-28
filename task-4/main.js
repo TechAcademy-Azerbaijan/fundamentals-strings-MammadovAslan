ca73627bd5a1dd1658010dbe55c535c6597ada8d
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-28 10:49:10 +0400

    task

--- task-4/main.js
+++ task-4/main.js
@@ -4,6 +4,15 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    let str = result.input
+
+    let counter = 0
+
+    for(let i = 0;i<str.length;i++){
+        if(str[i] === '!' || str[i] ==='.' || str[i] ==='?'){
+            counter++
+        }
+    }
+
+    console.log(counter);
 });
