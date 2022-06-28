f9d71954a7dd779b43783092ace5fa172e93e95b
Aslan Mammadov <105041099+MammadovAslan@users.noreply.github.com>
2022-06-28 11:31:25 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -4,6 +4,14 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    let str = result.input.split('');
+    //I am programming on Javascript. 
+    
+    for(let i = 0;i<str.length;i++){
+        if(str[i] === ' '){
+            console.log(str.indexOf(str[i]));
+            console.log(str.lastIndexOf(str[i]));
+            break
+        }
+    }
 });
