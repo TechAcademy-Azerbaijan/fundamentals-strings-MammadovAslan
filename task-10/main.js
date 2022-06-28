21de6c0023860786f39eee99034fa7537c5c45c3
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-28 18:31:23 +0400

    task

--- task-10/main.js
+++ task-10/main.js
@@ -7,22 +7,26 @@ prompt.get('input', function (err, result) {
   
     let str = result.input.split('');
     //I am programming on Javascript.
-    
-    for(let i = 0;i<str.length;i++){
-        if(str[i] === ' '){
-            lastInd = i
+    let firstInd = 0
+    let lastInd = 0
+    if(str.includes(' ')){
+        for(let i = 0;i<str.length;i++){
+            if(str[i] === ' '){
+                lastInd = i
+            }
         }
-    }
-    
-    for(let i = 0;i<str.length;i++){
-        if(str[i] === ' '){
-            firstInd = i
-            break
+        
+        for(let i = 0;i<str.length;i++){
+            if(str[i] === ' '){
+                firstInd = i
+                break
+            }
         }
-    }
-
-    console.log(firstInd);
-    console.log(lastInd);
+        console.log(firstInd);
+        console.log(lastInd);
+    }else{
+        console.log(-1);
+    }            
 });
 
 
