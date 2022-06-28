8a4e5c755313126ef488760df764c09a0355b543
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-28 10:41:39 +0400

    task

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,18 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    let num = result.input
+    let twoCount = 0
+    let fiveCount = 0
+    num = num.split('')
+
+    for(let i = 0;i<num.length;i++){
+        if(num[i] === "2"){
+            twoCount++
+        }else if(num[i] ==='5'){
+            fiveCount++
+        }
+    }
+
+    twoCount>fiveCount ? console.log('2') : console.log('5');
 });
